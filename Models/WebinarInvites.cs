bf907f6b2fd6689035024a1367f0aceb03806cd7
JagadeeshNani123 <virupakshijagadeeshreddyv@gmail.com>
2021-08-14 20:08:02 +0530

    BidBilly Conference Meeting Application

---
+++ Models/WebinarInvites.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Course.Models
+{
+    public class WebinarInvites
+    {
+        [Required(ErrorMessage="Please enter name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter Phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter valid Email Address")]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        
+        public bool? WillJoin { get; set; }
+    }
+}
